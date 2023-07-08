515bb57c4a92d60ccf7e42fd067d2491ec2c8eb5
PraDeePKuMaR <pradeepkumar24rk@gmail.com>
2023-07-08 22:22:41 +0530

    commit

--- src/Componets/main_component/frames/frame4/Frame4.jsx
+++ src/Componets/main_component/frames/frame4/Frame4.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import "./style.css"
 import Box from "../block_for_f4_f5/block.jsx"
 import Hu from "../../../../assets/humidity.svg"
@@ -6,12 +5,13 @@ import a from "../../../../assets/air.svg"
 import prec from "../../../../assets/prec.svg"
 import hum from "../../../../assets/hum.png"
 import pre from "../../../../assets/pre.png"
+import wind from '../../../../assets/chr.jpg'
 
 export default function Frame4() {
   return (
       <div className="f4_main">
         <Box text={"Humidity"} icon={<img src={Hu} alt='Hu'/>} graph={<p><img src={hum} alt="" width={150}/></p>}/>
-        <Box text={"Wind"} icon={<img src={a} alt='a'/>} graph={<p>graph</p>}/>
+        <Box text={"Wind"} icon={<img src={a} alt='a'/>} graph={<img src={wind} alt=""  width={150}/>}/>
         <Box text={"Precipitation"} icon={<img src={prec} alt='prec'/>} graph={<p><img src={pre} alt="" width={150} /></p>}/>
       </div>
     )
--- src/Componets/main_component/frames/frame5/Frame5.jsx
+++ src/Componets/main_component/frames/frame5/Frame5.jsx
@@ -4,13 +4,14 @@ import temp from "../../../../assets/temprature.svg"
 import rn from "../../../../assets/rain.svg"
 import uv from '../../../../assets/uv.png'
 import fl from '../../../../assets/feelf.png'
+import cr from '../../../../assets/changerain.png'
 
 export default function Frame5() {
   return (
       <div className="f4_main">
         <Box text={"UV index"} icon={<img src={sun} alt='sun'/>} graph={<p><img src={uv} alt=""  width={150}/></p>}/>
         <Box text={"Feels like"} icon={<img src={temp} alt='sun'/>} graph={<p><img src={fl} alt=""  width={150}/></p>}/>
-        <Box text={"Chance of rain"} icon={<img src={rn} alt='sun'/>} graph={<p>graph</p>}/>
+        <Box text={"Chance of rain"} icon={<img src={rn} alt='sun'/>} graph={<p><img src={cr} alt=""  width={150}/></p>}/>
       </div>
     )
 }
